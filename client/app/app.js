09fb63375ed634b884bcdb8cfd4de9546b2a3b01
michael moorman <michael.e.moorman@gmail.com>
2016-01-19 21:14:46 -0500

    removing crud, fixing submit button

--- client/app/app.js
+++ client/app/app.js
@@ -25,11 +25,7 @@ angular.module('doggr3App', [
     })
     .config(['flowFactoryProvider', function (flowFactoryProvider) {
         flowFactoryProvider.defaults = {
-            target: 'upload.php',
-            permanentErrors: [404, 500, 501],
-            maxChunkRetries: 1,
-            chunkRetryInterval: 5000,
-            simultaneousUploads: 4,
+
             singleFile: true
         };
         flowFactoryProvider.on('catchAll', function (event) {
